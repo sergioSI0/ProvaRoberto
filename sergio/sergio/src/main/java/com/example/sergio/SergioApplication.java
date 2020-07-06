82669fccc9982be5d3a066fd29d49a54878f23f3
sergioSI0 <48555559+sergioSI0@users.noreply.github.com>
2020-07-06 20:19:31 -0300

    Add files via upload

---
+++ sergio/sergio/src/main/java/com/example/sergio/SergioApplication.java
@@ -0,0 +1,13 @@
+package com.example.sergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergioApplication.class, args);
+	}
+
+}
